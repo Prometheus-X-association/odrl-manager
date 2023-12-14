dc8de3e6beaf84f33bedaa8ee3b3d54c03543584
mathieu <mathieu@visionspol.eu>
2023-12-14 17:46:16 +0100

    chore(): Add export for ContextFetcher.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { ContextFetcher, Custom } from 'ContextFetcher';
 import policyEvaluator, { PolicyEvaluator } from './PolicyEvaluator';
 import policyInstanciator, { PolicyInstanciator } from './PolicyInstanciator';
 
 export const evaluator: PolicyEvaluator = policyEvaluator;
 export const instanciator: PolicyInstanciator = policyInstanciator;
+export { ContextFetcher, Custom };
