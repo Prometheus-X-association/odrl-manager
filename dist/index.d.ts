9002da8b0ee0b4e883837b6f1674d6a6c28c2d43
mathieu <mathieu@visionspol.eu>
2023-12-14 09:36:53 +0100

    chore(): Update build.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,23 +1,106 @@
-declare abstract class PolicyValidator {
+interface LeftOperandFunctions {
+    absolutePosition: () => Promise<number>;
+    absoluteSize: () => Promise<number>;
+    absoluteSpatialPosition: () => Promise<[number, number]>;
+    absoluteTemporalPosition: () => Promise<Date>;
+    count: () => Promise<number>;
+    dateTime: () => Promise<Date>;
+    delayPeriod: () => Promise<number>;
+    deliveryChannel: () => Promise<string>;
+    device: () => Promise<string>;
+    elapsedTime: () => Promise<number>;
+    event: () => Promise<string>;
+    fileFormat: () => Promise<string>;
+    industry: () => Promise<string>;
+    language: () => Promise<string>;
+    media: () => Promise<string>;
+    meteredTime: () => Promise<number>;
+    payAmount: () => Promise<number>;
+    percentage: () => Promise<number>;
+    product: () => Promise<string>;
+    purpose: () => Promise<string>;
+    recipient: () => Promise<string>;
+    relativePosition: () => Promise<number>;
+    relativeSize: () => Promise<number>;
+    relativeSpatialPosition: () => Promise<[number, number]>;
+    relativeTemporalPosition: () => Promise<Date>;
+    resolution: () => Promise<number>;
+    spatial: () => Promise<string>;
+    spatialCoordinates: () => Promise<[number, number]>;
+    system: () => Promise<string>;
+    systemDevice: () => Promise<string>;
+    timeInterval: () => Promise<[Date, Date]>;
+    unitOfCount: () => Promise<string>;
+    version: () => Promise<string>;
+    virtualLocation: () => Promise<string>;
+    [key: string]: Function;
+}
+declare abstract class ContextFetcher {
+    context: LeftOperandFunctions;
+    constructor();
+    protected getAbsolutePosition(): Promise<number>;
+    protected getAbsoluteSize(): Promise<number>;
+    protected getAbsoluteSpatialPosition(): Promise<[number, number]>;
+    protected getAbsoluteTemporalPosition(): Promise<Date>;
+    protected getCount(): Promise<number>;
+    protected getDateTime(): Promise<Date>;
+    protected getDelayPeriod(): Promise<number>;
+    protected getDeliveryChannel(): Promise<string>;
+    protected getDevice(): Promise<string>;
+    protected getElapsedTime(): Promise<number>;
+    protected getEvent(): Promise<string>;
+    protected getFileFormat(): Promise<string>;
+    protected getIndustry(): Promise<string>;
+    protected getLanguage(): Promise<string>;
+    protected getMedia(): Promise<string>;
+    protected getMeteredTime(): Promise<number>;
+    protected getPayAmount(): Promise<number>;
+    protected getPercentage(): Promise<number>;
+    protected getProduct(): Promise<string>;
+    protected getPurpose(): Promise<string>;
+    protected getRecipient(): Promise<string>;
+    protected getRelativePosition(): Promise<number>;
+    protected getRelativeSize(): Promise<number>;
+    protected getRelativeSpatialPosition(): Promise<[number, number]>;
+    protected getRelativeTemporalPosition(): Promise<Date>;
+    protected getResolution(): Promise<number>;
+    protected getSpatial(): Promise<string>;
+    protected getSpatialCoordinates(): Promise<[number, number]>;
+    protected getSystem(): Promise<string>;
+    protected getSystemDevice(): Promise<string>;
+    protected getTimeInterval(): Promise<[Date, Date]>;
+    protected getUnitOfCount(): Promise<string>;
+    protected getVersion(): Promise<string>;
+    protected getVirtualLocation(): Promise<string>;
+}
+
+declare abstract class ModelEssential {
+    private static parentRelations;
+    protected static fetcher: ContextFetcher;
+    _objectUID: string;
+    constructor();
+    static setFetcher(fetcher: ContextFetcher): void;
+    setParent(parent: ModelEssential): void;
+    getParent(): ModelEssential;
     protected abstract verify(): Promise<boolean>;
-    protected validate(depth: number | undefined, validations: Promise<boolean>[]): void;
+    protected validate(depth: number | undefined, promises: Promise<boolean>[]): void;
     debug(depth?: number): void;
 }
 
-declare abstract class PolicyExplorer extends PolicyValidator {
+declare abstract class Explorable extends ModelEssential {
     protected abstract visit(): Promise<boolean>;
-    protected explore(depth: number | undefined, evaluators: Promise<boolean>[]): void;
+    protected explore(pick: Function, depth: number | undefined, entities: Explorable[], options?: any): void;
 }
 
-declare class ConflictTerm extends PolicyValidator {
+declare class ConflictTerm extends ModelEssential {
     verify(): Promise<boolean>;
 }
 
-declare class Party extends PolicyValidator {
+declare class Party extends ModelEssential {
     verify(): Promise<boolean>;
 }
 
-declare class LeftOperand extends PolicyValidator {
+declare class LeftOperand extends ModelEssential {
     private value;
     constructor(value: string);
     getValue(): string;
@@ -25,7 +108,7 @@ declare class LeftOperand extends PolicyValidator {
     verify(): Promise<boolean>;
 }
 
-declare class Operator extends PolicyValidator {
+declare class Operator extends ModelEssential {
     static readonly EQ: string;
     static readonly NEQ: string;
     static readonly GT: string;
@@ -43,13 +126,13 @@ declare class Operator extends PolicyValidator {
     verify(): Promise<boolean>;
 }
 
-declare class RightOperand extends PolicyValidator {
+declare class RightOperand extends ModelEssential {
     value: string | number;
     constructor(value: string | number);
     verify(): Promise<boolean>;
 }
 
-declare abstract class Constraint extends PolicyValidator {
+declare abstract class Constraint extends ModelEssential {
     uid?: string;
     dataType?: string;
     unit?: string;
@@ -64,7 +147,9 @@ declare abstract class Constraint extends PolicyValidator {
     protected verify(): Promise<boolean>;
 }
 
-declare class Action extends PolicyValidator {
+declare const actions: readonly ["Attribution", "CommericalUse", "DerivativeWorks", "Distribution", "Notice", "Reproduction", "ShareAlike", "Sharing", "SourceCode", "acceptTracking", "adHocShare", "aggregate", "annotate", "anonymize", "append", "appendTo", "archive", "attachPolicy", "attachSource", "attribute", "commercialize", "compensate", "concurrentUse", "copy", "delete", "derive", "digitize", "display", "distribute", "ensureExclusivity", "execute", "export", "extract", "extractChar", "extractPage", "extractWord", "give", "grantUse", "include", "index", "inform", "install", "lease", "lend", "license", "modify", "move", "nextPolicy", "obtainConsent", "pay", "play", "present", "preview", "print", "read", "reproduce", "reviewPolicy", "secondaryUse", "sell", "share", "shareAlike", "stream", "synchronize", "textToSpeech", "transfer", "transform", "translate", "uninstall", "use", "watermark", "write", "writeTo"];
+type ActionType = (typeof actions)[number];
+declare class Action extends ModelEssential {
     value: string;
     refinement?: Constraint[];
     includedIn: Action | null;
@@ -79,7 +164,7 @@ declare class AssetCollection extends Asset {
     refinement?: Constraint[];
 }
 
-declare class Asset extends PolicyValidator {
+declare class Asset extends Explorable {
     uid?: string;
     partOf?: AssetCollection[];
     hasPolicy?: string;
@@ -88,20 +173,21 @@ declare class Asset extends PolicyValidator {
         partOf?: AssetCollection[];
         hasPolicy?: string;
     });
+    protected visit(): Promise<boolean>;
     verify(): Promise<boolean>;
 }
 
 declare enum RelationType {
     TARGET = "target"
 }
-declare class Relation extends PolicyValidator {
+declare class Relation extends ModelEssential {
     type: RelationType;
     asset: Asset;
     constructor(type: RelationType, asset: Asset);
     verify(): Promise<boolean>;
 }
 
-declare abstract class Rule extends PolicyExplorer {
+declare abstract class Rule extends Explorable {
     action?: Action | Action[];
     target?: Asset;
     assigner?: Party;
@@ -113,7 +199,7 @@ declare abstract class Rule extends PolicyExplorer {
     uid?: string;
     relation?: Relation;
     constructor(uid?: string);
-    get constraints(): Constraint[];
+    protected get constraints(): Constraint[];
     setTarget(asset: Asset): void;
     setAction(action: Action): void;
     addAction(action: Action): void;
@@ -121,7 +207,6 @@ declare abstract class Rule extends PolicyExplorer {
     getTarget(): Asset | undefined;
     getAction(): Action | undefined | Action[];
     getConstraints(): Constraint[];
-    protected visit(): Promise<boolean>;
 }
 
 declare class RuleDuty extends Rule {
@@ -149,7 +234,7 @@ declare class RuleProhibition extends Rule {
     verify(): Promise<boolean>;
 }
 
-declare abstract class Policy extends PolicyExplorer {
+declare abstract class Policy extends Explorable {
     protected '@context': string;
     protected '@type': string;
     protected uid: string;
@@ -166,16 +251,44 @@ declare abstract class Policy extends PolicyExplorer {
     addPermission(permission: RulePermission): void;
     addProhibition(prohibition: RuleProhibition): void;
     addDuty(prohibition: RuleDuty): void;
-    launchValidation(): Promise<boolean>;
+    validate(): Promise<boolean>;
+    explore(picker: Function, options?: any): Promise<Explorable[]>;
 }
 
 declare class PolicyEvaluator {
     static instance: PolicyEvaluator;
+    private policies;
+    private readonly pickers;
     constructor();
     static getInstance(): PolicyEvaluator;
+    private pickTarget;
+    private pickPermission;
+    private pickProhibition;
+    cleanPolicies(): void;
+    addPolicy(policy: Policy): void;
     setPolicy(policy: Policy): void;
-    setDataContext(data: any): void;
-    visitTarget(target: string): Promise<void>;
+    setFetcher(fetcher: ContextFetcher): void;
+    private pick;
+    private explore;
+    /**
+     * Retrieves a list of performable actions on the specified target.
+     * @param target - A string representing the target
+     * @returns A promise resolved with an array of performables actions.
+     */
+    getPerformableActions(target: string): Promise<string[]>;
+    /**
+     * Verify if a specific action can be performed on a given target.
+     * @param actionType - A string representing the action.
+     * @param target - A string representing the target.
+     * @returns Resolves with a boolean indicating action performability.
+     */
+    isActionPerformable(actionType: ActionType, target: string): Promise<boolean>;
+    /**
+     * Evaluates the exploitability of listed resources within a set of policies.
+     * @param json - JSON representation of policies to be evaluated.
+     * @returns Resolves with a boolean indicating if the resources are exploitable.
+     */
+    evalResourcePerformabilities(json: any): Promise<boolean>;
 }
 
 declare class PolicyInstanciator {
@@ -184,15 +297,15 @@ declare class PolicyInstanciator {
     constructor();
     static getInstance(): PolicyInstanciator;
     private static readonly instanciators;
-    private static permission;
-    private static prohibition;
-    private static obligation;
-    private static duty;
-    private static action;
-    private static target;
-    private static constraint;
-    private static refinement;
-    private static consequence;
+    private static setPermission;
+    private static setProhibition;
+    private static setObligation;
+    private static setDuty;
+    private static setAction;
+    private static setTarget;
+    private static setConstraint;
+    private static setRefinement;
+    private static setConsequence;
     private selectPolicyType;
     genPolicyFrom(json: any): Policy | null;
     traverse(node: any, parent: any): void;
